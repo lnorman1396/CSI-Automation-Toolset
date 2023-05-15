565baef24e593c9630e9b7628c1c1fdbf0b661f3
lnorman1396 <110094747+lnorman1396@users.noreply.github.com>
2023-05-15 14:21:00 +0100

    Update new_script.py

--- Pages/Cleaner/new_script.py
+++ Pages/Cleaner/new_script.py
@@ -1,5 +1,37 @@
 import streamlit as st
 
 def run():
-    st.write('**TESTING Run Function**')
-    st.caption('directories and files will only be presented in the UI if there is content to present. i.e if the Collector subdirectory is empty, it will not appear in the select page UI ')
+   def convert_csv_to_excel(csv_data):
+        # Read the CSV data
+        df = pd.read_csv(io.StringIO(csv_data.decode('utf-8')))
+
+        # Perform your modifications here
+        # df = ...
+
+        # Create an in-memory Excel file
+        output = io.BytesIO()
+        with pd.ExcelWriter(output, engine='xlsxwriter') as writer:
+            df.to_excel(writer, sheet_name='Sheet1', index=False)
+        excel_data = output.getvalue()
+
+        return excel_data
+
+    def main():
+        st.title('CSV to Excel converter')
+
+        uploaded_file = st.file_uploader('Upload CSV file', type='csv')
+        if uploaded_file is not None:
+            st.write('Uploaded file:')
+            st.write(uploaded_file.name)
+
+            # Convert the uploaded CSV to Excel
+            excel_data = convert_csv_to_excel(uploaded_file.getvalue())
+
+            # Generate a download link for the Excel file
+            b64_excel = base64.b64encode(excel_data).decode()  # some strings
+            href = f'<a href="data:application/vnd.openxmlformats-officedocument.spreadsheetml.sheet;base64,{b64_excel}" download="output.xlsx">Download Excel file</a>'
+            st.markdown(href, unsafe_allow_html=True)
+        else:
+            st.write('Please upload a CSV file.')
+
+   
