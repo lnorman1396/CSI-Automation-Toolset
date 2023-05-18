61a5f6ae14097b0a4f6e125e4e215f5a396ebfa4
lnorman1396 <110094747+lnorman1396@users.noreply.github.com>
2023-05-18 16:17:41 +0100

    Update translator_JDF_to_OGTFS.py

--- Pages/Translator/translator_JDF_to_OGTFS.py
+++ Pages/Translator/translator_JDF_to_OGTFS.py
@@ -1,7 +1,4 @@
 from time import time
-import xlsxwriter
-
-
 import pandas as pd
 from pandas import read_csv, ExcelWriter, DataFrame, read_table
 from os.path import dirname, basename
@@ -496,7 +493,7 @@ def write_excel(table_array):
     logger.write('Writing excel output')
 
     output = io.BytesIO()
-    excel = pd.ExcelWriter(output, engine='xlsxwriter')
+    excel = pd.ExcelWriter(output)
     header_format = excel.book.add_format({'bold': True, 'text_wrap': False, 'align': 'left'})
 
     for i in table_array:
