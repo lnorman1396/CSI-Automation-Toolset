1ef94c807bd709db1d9f19426b0eec992654e606
lnorman1396 <110094747+lnorman1396@users.noreply.github.com>
2023-05-18 20:49:56 +0100

    Delete new_script.py

--- Pages/Translator/new_script.py
+++
@@ -1,5 +0,0 @@
-import streamlit as st
-
-def run():
-    st.write('**TESTING Run Function**')
-    st.caption('directories and files will only be presented in the UI if there is content to present. i.e if the Collector subdirectory is empty, it will not appear in the select page UI ')
