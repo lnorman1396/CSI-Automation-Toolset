a1bffabfe1467373cae7029a62cba221a8315ec2
lnorman1396 <110094747+lnorman1396@users.noreply.github.com>
2023-05-19 11:43:39 +0100

    Update main.py

--- main.py
+++ main.py
@@ -76,8 +76,8 @@ def home_page():
             </div>
             <p style="font-size: 0.8em; margin: 0; opacity: 0.7; font-style: italic;">Author: {author}</p>
         </div>
-        <p style="position: relative; height: 120px; overflow: hidden;">
-            <span style="position: absolute; line-height: 1.2em; max-height: 3.6em; display: inline-block; word-wrap: break-word; overflow: hidden;">{description}</span>
+        <p style="position: relative; height: 100px; overflow: hidden;">
+            <span style="position: absolute; line-height: 1.2em; max-height: 4.8em; display: inline-block; word-wrap: break-word; overflow: hidden;">{description}</span>
             <span style="position: absolute; bottom: 0; right: 0; padding-left: 10px; background-color: white;">...</span>
         </p>
     </div>
