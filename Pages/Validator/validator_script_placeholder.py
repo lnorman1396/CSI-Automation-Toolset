6c993450581e06614d80dbf4c134c28a3d69d65b
lnorman1396 <110094747+lnorman1396@users.noreply.github.com>
2023-05-19 11:03:14 +0100

    Update validator_script_placeholder.py

--- Pages/Validator/validator_script_placeholder.py
+++ Pages/Validator/validator_script_placeholder.py
@@ -1,5 +1,11 @@
 import streamlit as st
 
+class Description:
+    title = "JDf to OGTFS Converter"
+    description = "This is a script that converts a JDF zip file into OGTFS to enable you to import into optibus"
+    icon = "https://cdn-icons-png.flaticon.com/512/1322/1322164.png"
+
+
 def run():
     st.write('**TESTING Run Function**')
     st.caption('directories and files will only be presented in the UI if there is content to present. i.e if the Collector subdirectory is empty, it will not appear in the select page UI ')
