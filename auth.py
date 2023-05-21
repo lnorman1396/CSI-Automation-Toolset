615936eac8bb6ef701330a85a1ccc40f6783a69a
lnorman1396 <110094747+lnorman1396@users.noreply.github.com>
2023-05-21 09:20:48 +0100

    Update auth.py

--- auth.py
+++ auth.py
@@ -37,7 +37,8 @@ def auth():
         authorization_url, state = google.authorization_url(authorization_base_url)
 
         # Display login link
-        st.markdown(f'[Login with Google]({authorization_url})')
+        st.markdown(f'<a href="{authorization_url}" target="_parent">Login with Google</a>', unsafe_allow_html=True)
+        #st.markdown(f'[Login with Google]({authorization_url})')
 
 
         code = st.experimental_get_query_params().get("code")
