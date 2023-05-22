75cb049340be0b5a4193cb14c9d32ae862859be5
lnorman1396 <110094747+lnorman1396@users.noreply.github.com>
2023-05-22 10:08:47 +0100

    Update auth.py

--- auth.py
+++ auth.py
@@ -51,7 +51,7 @@ def auth():
         top_space_placeholder.markdown("<div style='padding: 15vh 0px;'></div>", unsafe_allow_html=True)
         login_card = f"""
         <div style="display: flex; flex-direction: column; align-items: center; justify-content: center; border: 1px solid #ddd; padding: 20px; border-radius: 10px; background-color: #fff;">
-            <h6>Automation Toolset</h6>
+            <h6 style="text-align: center;">Sign in: Automation Toolset</h6>
             <p style="font-size: 0.8em; margin: 0; opacity: 0.7; font-style: italic; text-align: center;">You must have an Optibus email account to sign into this application</p>
             <a href="{authorization_url}" style="display: inline-block; margin-top: 20px; padding: 10px 20px; background-color: #4285F4; color: #fff; text-decoration: none; border-radius: 5px;">Login with Google</a>
         </div>
