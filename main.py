ab892e9bb9f6060ef56f7a426a79bea7b9262afb
lnorman1396 <110094747+lnorman1396@users.noreply.github.com>
2023-05-22 12:42:28 +0100

    Update main.py

--- main.py
+++ main.py
@@ -100,8 +100,8 @@ def home_page():
                 ellipsis_html = '<span style="position: absolute; bottom: 0; right: 10px; padding-left: 10px; background-color: white;">...</span>' if is_description_too_long else ''
 
                 # Calculate font size based on the length of the description
-                if len(description) > 120:
-                    description = description[:120] + "..."
+                if len(description) > 110:
+                    description = description[:110] + "..."
 
                 cols[j].markdown(f"""
                             <div style="border: 1px solid #ddd; padding: 20px; margin-bottom: 10px; height: 180px; overflow: hidden; border-radius: 10px; background-color: #fff;">
@@ -112,7 +112,7 @@ def home_page():
                                     </div>
                                     <p style="font-size: 0.8em; margin: 0; opacity: 0.7; font-style: italic;">Author: {author}</p>
                                 </div>
-                                <p style="position: relative; font-size: 0.9em; height: 100px;">
+                                <p style="position: relative; font-size: 0.8em; height: 100px;">
                                     <span style="position: absolute; line-height: 1.2em; max-height: 4.8em; display: inline-block; word-wrap: break-word; padding-bottom:10px;">{description}</span>
                                 </p>
                             </div>
