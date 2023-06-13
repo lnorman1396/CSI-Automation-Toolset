f5cee8591b136fd5afa7570e8f8e53a8820dc024
jancoxd <51719888+jancoxd@users.noreply.github.com>
2023-06-13 18:01:50 +0200

    Update gtfs_deadhead_generator.py

--- Pages/Translator/gtfs_deadhead_generator.py
+++ Pages/Translator/gtfs_deadhead_generator.py
@@ -62,7 +62,7 @@ def run():
         lat_lon = stops[['stop_lat', 'stop_lon']].drop_duplicates()
 
         client = MapboxValhalla(api_key=api_key)
-        coords = [[lat, lon, ort_nr] for lat, lon, ort_nr in lat_lon.values.tolist()]
+        coords = [[lat, lon] for lat, lon in lat_lon.values.tolist()]
         combinations = pd.DataFrame([p for p in itertools.product(coords, repeat=2)])
         combinations = combinations[combinations[0] != combinations[1]]  # Exclude pairs with the same coordinates
         combinations = combinations.apply(lambda x: tuple(sorted([tuple(x[0]), tuple(x[1])])),
@@ -106,8 +106,8 @@ def run():
             download = 1
         # Retrieve the BytesIO object's content
         excel_data = output.getvalue()
-        
-        
+
+
         with excel_data as f:
             if download == 1:
                 st.download_button(
