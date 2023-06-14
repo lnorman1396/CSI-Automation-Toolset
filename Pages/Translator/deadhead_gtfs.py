9a69e51be3f1bb559809aff07fbb1980526a438c
jancoxd <51719888+jancoxd@users.noreply.github.com>
2023-06-14 12:08:11 +0200

    Update deadhead_gtfs.py

--- Pages/Translator/deadhead_gtfs.py
+++ Pages/Translator/deadhead_gtfs.py
@@ -39,7 +39,7 @@ def run():
         origin, destination = row[0], row[1]
         origin_lat, origin_lon = origin[1], origin[0]
         destination_lat, destination_lon = destination[1], destination[0]
-        time.sleep(0.5)
+        time.sleep(0.2)
         route = client.directions(locations=[origin, destination], profile='bus')
         origin_id = stops[(stops.stop_lat == origin_lat) & (
                 stops.stop_lon == origin_lon)].stop_id.values[0]
