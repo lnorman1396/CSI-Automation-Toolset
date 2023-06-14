646ddd13a717fb70773912ee03979500cf926b56
jancoxd <51719888+jancoxd@users.noreply.github.com>
2023-06-14 17:09:58 +0200

    Update deadhead_gtfs.py

--- Pages/Translator/deadhead_gtfs.py
+++ Pages/Translator/deadhead_gtfs.py
@@ -71,36 +71,44 @@ def run():
                         combinations[0] != combinations[1])]
         st.write(combinations.head(5))
         # combinations = combinations[(combinations[0] != combinations[1])]
-        try:
-            combinations[
-                ['Origin Stop Id', 'Destination Stop Id', 'Travel Time', 'Distance']] = combinations.apply(
-                lambda x: get_routing(x, stops), axis=1, result_type='expand')
-    
-        except Exception as e:
-            st.write(e)
-            pass
-    
-        st.write('Combinations finished')
-        columns = ['Start Time Range', 'End Time Range', '	Generate Time', 'Route Id', 'Origin Stop Name',
+        results = []
+
+        st.write("Progress")
+        progress_bar = st.progress(0)
+        total_combinations = len(combinations)
+
+        for i, row in combinations.iterrows():
+            try:
+                result = get_routing(row, client)
+            except Exception as e:
+                pass
+            results.append(result)
+
+            # Update the progress bar
+            try:
+                progress_bar.progress((i + 1) / (total_combinations + 1))
+
+            except Exception as e:
+                pass
+
+        results_df = pd.DataFrame(results, columns=['Origin Stop Id', 'Destination Stop Id', 'Travel Time', 'Distance'])
+
+        columns = ['Start Time Range', 'End Time Range', 'Generate Time', 'Route Id', 'Origin Stop Name',
                    'Destination Stop Name',
                    'Days Of Week', 'Direction', 'Purpose', 'Alignment', 'Pre-Layover Time', 'Post-Layover Time',
                    'updatedAt']
-        st.write('Columns finished')
-    
-        combinations = pd.concat([combinations, pd.DataFrame(columns=columns)])
-        st.write('Combinations concat finished')
-    
-        st.write('Combinations drop finished')
-        combinations = combinations.drop([0, 1, 'crow_distance'], axis=1)
+
+        combinations = pd.concat([results_df, pd.DataFrame(columns=columns)])
+
         # Write DataFrame to BytesIO object
         output = io.BytesIO()
-    
+
         with pd.ExcelWriter(output, engine='openpyxl') as writer:
             combinations.to_excel(writer, index=False, sheet_name='Deadheads')
-    
+
         # Retrieve the BytesIO object's content
         excel_data = output.getvalue()
-    
+
         st.write('Excel finished')
         download = 1
         if download == 1:
