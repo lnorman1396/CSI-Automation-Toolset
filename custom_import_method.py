c5b1f0615bfa52aca7897e7ddaf55d22426d8e59
lnorman1396 <110094747+lnorman1396@users.noreply.github.com>
2023-06-20 14:24:51 +0100

    Update custom_import_method.py

--- custom_import_method.py
+++ custom_import_method.py
@@ -11,19 +11,8 @@ def _import(full_name, error_messages):
 
         return module
     except ImportError as e:
-        error_messages.append(f"Failed to import: {full_name}")
+        
         return None
     except AttributeError as e:
-        error_messages.append(f"Failed to import: {parts}")
+       
         return None
-
-def import_with_logs(full_name):
-    error_messages = []
-    module = _import(full_name, error_messages)
-
-    if error_messages:
-        expander = st.sidebar.expander('log')
-        for msg in error_messages:
-            expander.write(msg)
-
-    return module
