e50fca728a7e9958a44e19892ebf077edadf0247
lnorman1396 <110094747+lnorman1396@users.noreply.github.com>
2023-06-20 14:11:04 +0100

    Update optibus_api_comparison.py

--- Pages/Comparison/optibus_api_comparison.py
+++ Pages/Comparison/optibus_api_comparison.py
@@ -1,26 +1,4 @@
-
-
-
-
-def _import(full_name):
-    try:
-        parts = full_name.split('.')
-        module = __import__(parts[0])
-
-        for part in parts[1:]:
-            module = getattr(module, part)
-
-        return module
-    except ImportError as e:
-        expander = st.sidebar.expander('log') 
-        expander.write(f"Failed to import: {full_name}")
-        return None
-    except AttributeError as e:
-        expander = st.sidebar.expander('log') 
-        expander.write(f"Failed to import: {parts}")
-        return None
-
-
+from custom_import_method import _import
 
 st = _import('streamlit')
 requests = _import('requests')
