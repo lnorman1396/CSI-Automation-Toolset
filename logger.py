9f0502e9a27412f3c5d7f7237de58e406c5da6a6
lnorman1396 <110094747+lnorman1396@users.noreply.github.com>
2023-06-20 12:31:20 +0100

    Update logger.py

--- logger.py
+++ logger.py
@@ -9,19 +9,29 @@ class StreamlitLogger(logging.Logger):
         super().__init__(name)
 
 class StreamlitHandler(logging.Handler):
+    def __init__(self):
+        super().__init__()
+        self.log_messages = []
+
     def emit(self, record):
-        log_message = self.format(record)
-        # append log_message to log_messages list
-        log_messages.append(log_message)
+        log_entry = self.format(record)
+        self.log_messages.append(log_entry)
+
+    def get_logs(self):
+        return self.log_messages
+
+    def clear_logs(self):
+        self.log_messages = []
 
 # Function to get the logger
 def get_logger(name):
-    logger = StreamlitLogger(name)
+    logger = logging.getLogger(name)
+    logger.handlers = []  # Remove all existing handlers
+    logger.setLevel(logging.INFO)
     handler = StreamlitHandler()
-    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-    handler.setFormatter(formatter)
+    handler.setFormatter(logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s'))
     logger.addHandler(handler)
-    return logger
+    return logger, handler
 
 # Function to get the log messages
 def get_log_messages():
