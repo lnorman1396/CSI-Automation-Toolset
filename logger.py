f5f573b79708568c7c555ed0d4390f630d72f81c
lnorman1396 <110094747+lnorman1396@users.noreply.github.com>
2023-06-20 11:51:38 +0100

    Update logger.py

--- logger.py
+++ logger.py
@@ -1,1 +1,14 @@
+# logger.py
+import logging
+import streamlit as st
 
+class StreamlitLogger(logging.Logger):
+    def __init__(self, name):
+        super().__init__(name)
+
+    def emit(self, record):
+        st.write(self.format(record))
+
+# Function to get the logger
+def get_logger(name):
+    return StreamlitLogger(name)
