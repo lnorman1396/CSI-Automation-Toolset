e9e5f3e15dec4383bb59157b947c45f65c4d2505
lnorman1396 <110094747+lnorman1396@users.noreply.github.com>
2023-06-20 14:52:50 +0100

    Update custom_import_converter.py

--- Pages/Translator/custom_import_converter.py
+++ Pages/Translator/custom_import_converter.py
@@ -13,26 +13,28 @@ class Description:
 
 def run():
 
-    def process_imports(imports):
-        lines = imports.split('\n')
-        new_imports = ["from custom_import_method import _import"]
-        for line in lines:
-            if line.startswith('import'):
-                modules = line[7:].split(',')
-                for module in modules:
-                    module = module.strip()
-                    new_imports.append(f'{module} = _import("{module}")')
-            elif line.startswith('from'):
-                parts = line.split(' ')
-                module = parts[1]
-                submodules = parts[3].split(',')
-                for sub in submodules:
-                    sub = sub.strip()
-                    new_imports.append(f'{sub} = _import("{module}.{sub}")')
-        return '\n'.join(new_imports)
+    def process_imports(raw_imports):
+    lines = raw_imports.split("\n")
+    processed_imports = []
+
+    for line in lines:
+        # Handle import with alias
+        if " as " in line:
+            alias, package = line.split(" as ")
+            processed_imports.append(f"{alias.strip()} = _import('{package.strip()}')")
+        # Handle normal import
+        elif "import " in line:
+            package = line.split("import ")[-1]
+            processed_imports.append(f"{package.strip()} = _import('{package.strip()}')")
+        # Handle from-import statement
+        elif "from " in line:
+            path, name = line.split("from ")[-1].split(" import ")
+            processed_imports.append(f"{name.strip()} = _import('{path.strip()}.{name.strip()}')")
+
+    return "\n".join(processed_imports)
 
     user_input = st.text_area("Paste your standard Python import statements here:", height=200, placeholder='import streamlit as st')
 
     if st.button('Convert'):
         result = process_imports(user_input)
-        st.code(result)
+        st.code(f"from custom_import_method import _import\n\n{result})
