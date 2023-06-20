046bc8ffbdd9e3dce1e1fe9ec0a1a523e6550ab5
lnorman1396 <110094747+lnorman1396@users.noreply.github.com>
2023-06-20 14:38:08 +0100

    Update custom_import_converter.py

--- Pages/Translator/custom_import_converter.py
+++ Pages/Translator/custom_import_converter.py
@@ -1,8 +1,17 @@
 
 import streamlit as st
 
+class Instructions:
+    instructions = 'Enter the depot name, Paste in schedule 1 and schedule 2, check the domain is supported by the api call, run the script'
+    link = '#'
+
+class Description:
+    title = "Schedule Comparison"
+    description = "This is a script that uses the Optibus API amd compares schedules from optibus urls to create an excel file, highlighting the savings between scenarios"
+    icon = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRmaLtoRAJT4nxBOCBs_mQapmWJv3gxDjaYIQ&usqp=CAU"
+    author = 'Luke Norman'
+
 def run():
-    st.title('Python Import Converter')
 
     def process_imports(imports):
         lines = imports.split('\n')
@@ -22,7 +31,7 @@ def run():
                     new_imports.append(f'{sub} = _import("{module}.{sub}")')
         return '\n'.join(new_imports)
 
-    user_input = st.text_area("Paste your standard Python import statements here:")
+    user_input = st.text_area("Paste your standard Python import statements here:", height=100, placeholder='import streamlit as st')
 
     if st.button('Convert'):
         result = process_imports(user_input)
