5e7f3f04b3cf10665ab005c6c55117503bc4f385
lnorman1396 <110094747+lnorman1396@users.noreply.github.com>
2023-06-20 14:15:32 +0100

    Update custom_import_method.py

--- custom_import_method.py
+++ custom_import_method.py
@@ -1,9 +1,6 @@
 # logger.py
 import streamlit as st
-
-def _import(full_name):
-    import streamlit as st
-
+def _import(full_name, expander):
     try:
         parts = full_name.split('.')
         module = __import__(parts[0])
@@ -13,10 +10,8 @@ def _import(full_name):
 
         return module
     except ImportError as e:
-        expander = st.sidebar.expander('log') 
         expander.write(f"Failed to import: {full_name}")
         return None
     except AttributeError as e:
-        expander = st.sidebar.expander('log') 
         expander.write(f"Failed to import: {parts}")
         return None
