8e3b27f0668239587c5e79d80a36112b5700d5cd
lnorman1396 <110094747+lnorman1396@users.noreply.github.com>
2023-06-20 12:07:45 +0100

    Update logger.py

--- logger.py
+++ logger.py
@@ -9,15 +9,16 @@ class StreamlitHandler(logging.Handler):
 
 def get_logger(name):
     logger = logging.getLogger(name)
-    logger.setLevel(logging.DEBUG)
 
-    # Check if StreamlitHandler is already in logger.handlers
-    if not any(isinstance(handler, StreamlitHandler) for handler in logger.handlers):
-        handler = StreamlitHandler()
-        handler.setLevel(logging.INFO)
-        formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-        handler.setFormatter(formatter)
-        logger.addHandler(handler)
-        logger.propagate = False
+    if (logger.hasHandlers()):
+        logger.handlers.clear()
+
+    logger.setLevel(logging.DEBUG)
+    handler = StreamlitHandler()
+    handler.setLevel(logging.INFO)
+    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+    handler.setFormatter(formatter)
+    logger.addHandler(handler)
+    logger.propagate = False
 
     return logger
