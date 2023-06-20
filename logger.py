dfd68410bd80ede6f73d65fa71a5ef9b28d8f909
lnorman1396 <110094747+lnorman1396@users.noreply.github.com>
2023-06-20 12:00:47 +0100

    Update logger.py

--- logger.py
+++ logger.py
@@ -2,13 +2,31 @@
 import logging
 import streamlit as st
 
-class StreamlitLogger(logging.Logger):
-    def __init__(self, name):
-        super().__init__(name)
-
+class StreamlitHandler(logging.Handler):
     def emit(self, record):
-        st.write(self.format(record))
+        msg = self.format(record)
+        st.write(msg)
 
 # Function to get the logger
 def get_logger(name):
-    return StreamlitLogger(name)
+    # Create a logger
+    logger = logging.getLogger(name)
+    # We set the logger level to the "lowest" possible, 
+    # as level control will be handled by the handler
+    logger.setLevel(logging.DEBUG)
+
+    # Create our custom handler
+    streamlit_handler = StreamlitHandler()
+    streamlit_handler.setLevel(logging.INFO)
+
+    # We create a formatter
+    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+
+    # We link the formatter to our handler
+    streamlit_handler.setFormatter(formatter)
+
+    # We link the handler to our logger
+    logger.addHandler(streamlit_handler)
+
+    return logger
+
