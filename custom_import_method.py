10b69efc32f37a89e21ac02ad7af5d2e0845eea4
lnorman1396 <110094747+lnorman1396@users.noreply.github.com>
2023-06-20 14:10:47 +0100

    Update custom_import_method.py

--- custom_import_method.py
+++ custom_import_method.py
@@ -1,40 +1,22 @@
 # logger.py
-import logging
 import streamlit as st
 
-log_messages = []
+def _import(full_name):
+    import streamlit as st
 
-class StreamlitLogger(logging.Logger):
-    def __init__(self, name):
-        super().__init__(name)
+    try:
+        parts = full_name.split('.')
+        module = __import__(parts[0])
 
-class StreamlitHandler(logging.Handler):
-    def __init__(self):
-        super().__init__()
-        self.log_messages = []
+        for part in parts[1:]:
+            module = getattr(module, part)
 
-    def emit(self, record):
-        log_entry = self.format(record)
-        self.log_messages.append(log_entry)
-
-    def get_logs(self):
-        return self.log_messages
-
-    def clear_logs(self):
-        self.log_messages = []
-
-# Function to get the logger
-def get_logger(name):
-    logger = logging.getLogger(name)
-    logger.handlers = []  # Remove all existing handlers
-    logger.setLevel(logging.INFO)
-    handler = StreamlitHandler()
-    handler.setFormatter(logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s'))
-    logger.addHandler(handler)
-    return logger, handler
-
-# Function to get the log messages
-def get_log_messages():
-    # return log_messages
-    return log_messages
-
+        return module
+    except ImportError as e:
+        expander = st.sidebar.expander('log') 
+        expander.write(f"Failed to import: {full_name}")
+        return None
+    except AttributeError as e:
+        expander = st.sidebar.expander('log') 
+        expander.write(f"Failed to import: {parts}")
+        return None
